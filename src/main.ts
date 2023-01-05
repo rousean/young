e6d5c1b791802ebf84ff7f7176bf67f5d2d04421
rousean <rousean@163.com>
2023-01-05 13:37:27 +0800

    young

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import '@/scss/main.scss'
